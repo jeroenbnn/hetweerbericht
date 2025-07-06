243f1d545c3e3e2944e769906bac4578246017fa
Jeroen Boonen <76564930+jeroenbnn@users.noreply.github.com>
2025-07-06 07:12:15 +0200

    Update buienradar.js

--- src/js/buienradar.js
+++ src/js/buienradar.js
@@ -1,1 +1,8 @@
-
+export const buienradarImage = async (data, key) => {
+  if (data.lat && data.lon) {
+    locationFrame = `<iframe src="https://gadgets.buienradar.nl/gadget/zoommap/?lat=${data.lat}&lng=${data.lon}&overname=2&zoom=8&size=3&voor=0" scrolling=no width=550 height=512 frameborder=no></iframe>`;
+  } else {
+    locationFrame = `<iframe src="https://gadgets.buienradar.nl/gadget/zoommap/?lat=50.10&lng=4.29&overname=2&zoom=8&size=3&voor=0" scrolling=no width=550 height=512 frameborder=no></iframe>`;
+  }
+}
+  
