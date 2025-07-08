ac14d1caf7c58931ebb7af197665af21b14cf160
Jeroen Boonen <76564930+jeroenbnn@users.noreply.github.com>
2025-07-08 19:08:54 +0200

    Update weatherForecastCards.js

--- src/js/weatherForecastCards.js
+++ src/js/weatherForecastCards.js
@@ -20,10 +20,11 @@ export const createHourlyCards = () => {
     hourlyWeatherForecastTemperature.classList.add("hourly-weather-forecast-temperature", "loading", "dynamic-data");
     hourlyWeatherForecastTemperature.innerHTML = "&emsp;&emsp;";
 
+    /*
     const hourlyWeatherForecastWindspeed = document.createElement("div");
     hourlyWeatherForecastWindspeed.classList.add("daily-weather-forecast-windspeed", "loading", "dynamic-data");
     hourlyWeatherForecastWindspeed.innerHTML = "&emsp;&emsp;";
-
+    */
     hourlyWeatherForecastDateTime.appendChild(hourlyWeatherForecastDate);
     hourlyWeatherForecastDateTime.appendChild(hourlyWeatherForecastTime);
 
